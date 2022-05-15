39a74c2298f412231bf492fd2af6883c290ca85b
Collin Kinchen <collinkinchen@gmail.com>
2022-05-15 12:53:11 -0700

    First commit

---
+++ myproject/models.py
@@ -0,0 +1,13 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin,db.Model):
+    __table_args__ = {'extend_existing': True} 
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    fav_food = db.Column(db.String(1000))
+
+
